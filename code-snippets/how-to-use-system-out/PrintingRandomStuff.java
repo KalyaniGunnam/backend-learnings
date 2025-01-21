c628d01732fca65cedf9448255b066f791f1c2da
KalyaniGunnam <112619574+KalyaniGunnam@users.noreply.github.com>
2025-01-21 13:40:17 +0530

    Update PrintingRandomStuff.java

--- code-snippets/how-to-use-system-out/PrintingRandomStuff.java
+++ code-snippets/how-to-use-system-out/PrintingRandomStuff.java
@@ -1,5 +1,11 @@
-public class HowToUseSystemOut {
+public class PrintingRandomStuff {
     public static void main(String[] args) {
-        
+        // With System.out.print() method
+//        System.out.print("I like pizza!");
+//        System.out.print("It’s really delicious!");
+
+        // With System.out.println() method
+        System.out.println("I like pizza!");
+        System.out.println("It’s really delicious!");
     }
 }
